8fc2823a8aa8d02abb0bd6f734bf4cb077c77a74
Javad <mmkismylove@gmail.com>
2024-03-27 13:39:17 +0100

    reallive2time

--- server.js
+++ server.js
@@ -1,49 +1,48 @@
+const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
-require('dotenv').config(); // Load environment variables
-const express = require('express');
 const mongoose = require('mongoose');
-const messageRoutes = require('./routes/messageRoutes');
 const cors = require('cors');
-const userRoutes = require('./routes/userRoutes'); // Ensure this path matches your project structure
+const messageRoutes = require('./routes/messageRoutes');
+const userRoutes = require('./routes/userRoutes');
 
-const app = express(); // Define the app before using it to create the server
+require('dotenv').config();
 
-const server = http.createServer(app); // Use app to create the server
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:3000", "https://mern-task-appjavad.onrender.com"],
-    methods: ["GET", "POST"],
-  }
-});
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-
-  socket.on('sendMessage', (message) => {
-    io.emit('receiveMessage', message); // Emit the message to all connected clients
-  });
-});
-
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(userRoutes);
-app.use(messageRoutes);
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:3000", "https://mern-task-appjavad.onrender.com"],
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    
+    socket.on('sendMessage', (message) => {
+        io.emit('receiveMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+app.use('/api', userRoutes);
+app.use('/api', messageRoutes);
 
 mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Could not connect to MongoDB:', err));
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Could not connect to MongoDB:', err));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// Use server.listen instead of app.listen
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
