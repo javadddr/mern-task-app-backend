f6db83c10c4563342b5253323a56ecace1b17671
Javad <mmkismylove@gmail.com>
2024-03-27 13:56:13 +0100

    default

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const http = require('http');
-const { Server } = require("socket.io");
 require('dotenv').config(); // Load environment variables
 const express = require('express');
 const mongoose = require('mongoose');
@@ -7,44 +5,32 @@ const messageRoutes = require('./routes/messageRoutes');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes'); // Ensure this path matches your project structure
 
-const app = express(); // Define the app before using it to create the server
+const app = express();
 
-const server = http.createServer(app); // Use app to create the server
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:3000", "https://mern-task-appjavad.onrender.com"],
-    methods: ["GET", "POST"],
-  }
-});
+// Use CORS middleware to allow cross-origin requests
+app.use(cors({
+    origin:["http://localhost:3000","https://mern-task-appjavad.onrender.com"]
+}));
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-
-  socket.on('sendMessage', (message) => {
-    io.emit('receiveMessage', message); // Emit the message to all connected clients
-  });
-});
-
-app.use(cors());
+// Middleware to parse JSON
 app.use(express.json());
 
+// Use user routes
 app.use(userRoutes);
 app.use(messageRoutes);
-
+// Connect to MongoDB
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Could not connect to MongoDB:', err));
 
+// Define a simple route for testing
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// Use server.listen instead of app.listen
+// Listen on a port
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
