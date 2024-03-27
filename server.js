93fb0235cd24bbbd866a188eead4492496f37c34
Javad <mmkismylove@gmail.com>
2024-03-27 13:20:36 +0100

    real

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const http = require('http');
+const { Server } = require("socket.io");
 require('dotenv').config(); // Load environment variables
 const express = require('express');
 const mongoose = require('mongoose');
@@ -5,32 +7,44 @@ const messageRoutes = require('./routes/messageRoutes');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes'); // Ensure this path matches your project structure
 
-const app = express();
+const app = express(); // Define the app before using it to create the server
 
-// Use CORS middleware to allow cross-origin requests
-app.use(cors({
-    origin:["http://localhost:3000","https://mern-task-appjavad.onrender.com"]
-}));
+const server = http.createServer(app); // Use app to create the server
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000", "https://mern-task-appjavad.onrender.com"],
+    methods: ["GET", "POST"],
+  }
+});
 
-// Middleware to parse JSON
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('sendMessage', (message) => {
+    io.emit('receiveMessage', message); // Emit the message to all connected clients
+  });
+});
+
+app.use(cors());
 app.use(express.json());
 
-// Use user routes
 app.use(userRoutes);
 app.use(messageRoutes);
-// Connect to MongoDB
-// Connect to MongoDB
+
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Could not connect to MongoDB:', err));
 
-// Define a simple route for testing
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// Listen on a port
+// Use server.listen instead of app.listen
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
